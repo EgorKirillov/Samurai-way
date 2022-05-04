2fbee3098f8fc5dc73e2debd6b0d474bdbf9c3a1
EgorKirillov <80296253508@mail.ru>
2022-05-04 18:01:15 +0300

    18 lesson complite

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+
 
 function App() {
-  return (
-    <div className={'app-writer'}>
-     Hi I'll be back
-        <Header />
-        <Navbar />
-        <Profile />
-    </div>
-  );
+    return (
+        <div className={'app-wrapper'}>
+
+            <Header/>
+            <Navbar/>
+
+            <div className={'app-wrapper-content'}>
+                <Dialogs />
+                {/*<Profile />*/}
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Dialogs.module.css'
+
+export const Dialogs = () => {
+    return (
+        <div className={s.content}>
+dialogs
+        </div>
+    );
+}
+
