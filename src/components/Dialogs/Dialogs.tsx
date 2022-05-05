a6b0cb4ff6b56785d7c07566d2ccc0a06dacf6c4
EgorKirillov <80296253508@mail.ru>
2022-05-05 13:33:24 +0300

    lesson 21 complite

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,29 @@ import s from './Dialogs.module.css'
 export const Dialogs = () => {
     return (
         <div className={s.content}>
-dialogs
+            <div className={s.dialogList}>
+                <div className={s.dialog}> User 1 </div>
+                <div className={s.dialog}> User 2 </div>
+                <div className={s.dialog}> User 3 </div>
+                <div className={s.dialog}> User 4 </div>
+                <div className={s.dialog}> User 5 </div>
+                <div className={s.dialog}> User 6 </div>
+
+            </div>
+
+            <div className={s.messageList}>
+                <div className={s.message}> message 1 </div>
+                <div className={s.message}> message 2 </div>
+                <div className={s.message}> message 3 </div>
+                <div className={s.message}> message 4 </div>
+                <div className={s.message}> message 5 </div>
+                <div className={s.message}> message 6 </div>
+
+            </div>
+
+
+
+
         </div>
     );
 }
