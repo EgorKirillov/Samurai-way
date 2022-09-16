6fe18e4625da2663d9923bedf6af67cdcd8bc774
EgorKirillov <80296253508@mail.ru>
2022-09-16 12:45:27 +0300

    refactor: convert navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,26 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.css"
 
-export function Navbar() {
+export const Navbar = () => {
     return (
         <nav className={s.nav} >
             <div className={s.item}>
-                <NavLink to={"/profile"} activeClassName={s.active}>Profile</NavLink>
+                <NavLink key={"profile"} to={"/profile"} activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to={"/dialogs"} activeClassName={s.active}>Messages</NavLink>
+                <NavLink key={"dialogs"} to={"/dialogs"} activeClassName={s.active}>Messages</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <NavLink to={"/news"} activeClassName={s.active}>News</NavLink>
+                <NavLink key={"news"} to={"/news"} activeClassName={s.active}>News</NavLink>
             </div>
            <div className={`${s.item} ${s.active}`}>
-              <NavLink to={"/users"} activeClassName={s.active}>Users</NavLink>
+              <NavLink key={"users"} to={"/users"} activeClassName={s.active}>Users</NavLink>
            </div>
            <div className={s.item}>
-                <NavLink to={"/music"} activeClassName={s.active}>Music</NavLink>
+                <NavLink key={"music"} to={"/music"} activeClassName={s.active}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to={"/settings"} activeClassName={s.active}>Settings</NavLink>
+                <NavLink key={"settings"} to={"/settings"} activeClassName={s.active}>Settings</NavLink>
             </div>
 
         </nav>
