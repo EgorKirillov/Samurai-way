f0fb5f78b13a44bd70328520ea2ca337858b121b
EgorKirillov <80296253508@mail.ru>
2022-11-14 21:03:42 +0300

    fix: link error

--- src/components/Dialogs/DialogsItms/DialogItems.tsx
+++ src/components/Dialogs/DialogsItms/DialogItems.tsx
@@ -6,7 +6,10 @@ import { DialogItemTypes } from '../../../Redux/dialogsReducer'
 export const DialogItem = (props: DialogItemTypes) => {
   return (
     <div className={s.dialog}>
-      <img src={require('../../../avatar/' + props.avatarLink)} alt={'avatar' + props.avatarLink} />
+      <img
+        src={require('../../../assets/avatar/' + props.avatarLink)}
+        alt={'avatar' + props.avatarLink}
+      />
       <NavLink to={'/dialogs/' + props.linkID} activeClassName={s.dialogListActive}>
         {' '}
         {props.userName}{' '}
